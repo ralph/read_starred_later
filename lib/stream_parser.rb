66efc6d8dd51901dd8411acb94d3eea5b76c3640
ralph <ralph@rvdh.de>
2010-04-12 00:19:57 +0200

    adjusted the stream parser a little

--- lib/stream_parser.rb
+++ lib/stream_parser.rb
@@ -3,6 +3,8 @@ require "bundler"
 Bundler.setup
 
 require 'tweetstream'
+require 'yaml'
+require File.expand_path('..', __FILE__) + '/instapaper'
 
 begin
   config = YAML.load_file('config.yml')
@@ -11,8 +13,16 @@ rescue Errno::ENOENT
   Process.exit
 end
 
-users = YAML.load_file('data/users.yml')
+users = YAML.load_file('data/users.yml').keys
 
 TweetStream::Client.new(config[:user], config[:password]).follow(*users) do |status|
+  if status.favorited
+    user = users[status.user.id]
+    username = user[:username]
+    password = user[:password]
+    Instapaper.add_url(username, password, 'http://www.heise.de')
+  end
+
   puts "#{status.text}"
+  puts "#{status.inspect}"
 end
